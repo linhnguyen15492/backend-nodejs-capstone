8092937abd67cf8f50822714baf7e55cd4e49c6d
linhnguyen15492 <honglinh154@gmail.com>
2025-12-17 15:42:36 +0700

    style: reformat code

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,5 +1,3 @@
-/*jshint esversion: 8 */
-
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -7,7 +5,7 @@ const pinoLogger = require('./logger')
 const path = require('path')
 
 const connectToDatabase = require('./models/db')
-const { loadData } = require('./util/import-mongo/index')
+// const { loadData } = require('./util/import-mongo/index')
 
 const app = express()
 app.use('*', cors())
